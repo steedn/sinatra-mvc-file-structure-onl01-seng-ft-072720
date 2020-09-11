7cb9ce6da46d65d74945e9f55d52c686c3b5ebfd
Nathan Steed <steedn@outlook.com>
2020-09-11 23:12:09 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,13 @@
 class Dog
+  @@all = []
   attr_accessor :name, :breed, :age
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
+    save
+  end
+  def self.save
+    @@all << self
   end
 end
